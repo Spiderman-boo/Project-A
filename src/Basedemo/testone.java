70d50ff2e98d9d6bf8b6bd866789347b0efe082f
Spiderman-boo <sandeepyadla596@gmail.com>
2024-11-03 17:06:25 +0530

    from A

---
+++ src/Basedemo/testone.java
@@ -0,0 +1,14 @@
+package Basedemo;
+
+public class testone {
+	public static void main(String[]args)
+	{
+		System.out.println("hello word ");
+		System.out.println("hello word ");
+		System.out.println("hello word ");
+		System.out.println("hello word ");
+		System.out.println("hello word ");
+		
+	}
+
+}
